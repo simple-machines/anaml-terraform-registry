9919316a0e7f06d08ea023aab853ae30edc2f5b1
Adam Evans <aevans@simplemachines.com.au>
2022-07-05 15:00:00 +1000

    Updated GKE cluster outputs with descriptions imported from the source module

--- modules/gcp-anaml-gke-cluster/outputs.tf
+++ modules/gcp-anaml-gke-cluster/outputs.tf
@@ -1,71 +1,91 @@
 output "ca_certificate" {
-  value = module.gke_cluster.ca_certificate
+  sensitive   = true
+  description = "Cluster ca certificate (base64 encoded)"
+  value       = module.gke_cluster.ca_certificate
 }
 
 output "cluster_id" {
-  value = module.gke_cluster.cluster_id
+  description = "Cluster ID"
+  value       = module.gke_cluster.cluster_id
 }
 
 output "endpoint" {
-  value = module.gke_cluster.endpoint
+  sensitive   = true
+  description = "Cluster endpoint"
+  value       = module.gke_cluster.endpoint
 }
 
 output "horizontal_pod_autoscaling_enabled" {
-  value = module.gke_cluster.horizontal_pod_autoscaling_enabled
+  description = "Whether horizontal pod autoscaling enabled"
+  value       = module.gke_cluster.horizontal_pod_autoscaling_enabled
 }
 
 output "http_load_balancing_enabled" {
-  value = module.gke_cluster.http_load_balancing_enabled
+  description = "Whether http load balancing enabled"
+  value       = module.gke_cluster.http_load_balancing_enabled
 }
 
 output "identity_namespace" {
-  value = module.gke_cluster.identity_namespace
+  description = "Workload Identity pool"
+  value       = module.gke_cluster.identity_namespace
 }
 
 output "instance_group_urls" {
-  value = module.gke_cluster.instance_group_urls
+  description = "List of GKE generated instance groups"
+  value       = module.gke_cluster.instance_group_urls
 }
 
 output "location" {
-  value = module.gke_cluster.location
+  description = "Cluster location (region if regional cluster, zone if zonal cluster)"
+  value       = module.gke_cluster.location
 }
 
 output "logging_service" {
-  value = module.gke_cluster.logging_service
+  description = "Logging service used"
+  value       = module.gke_cluster.logging_service
 }
 
 output "master_version" {
-  value = module.gke_cluster.master_version
+  description = "Current master kubernetes version"
+  value       = module.gke_cluster.master_version
 }
 
 output "min_master_version" {
-  value = module.gke_cluster.min_master_version
+  description = "Minimum master kubernetes version"
+  value       = module.gke_cluster.min_master_version
 }
 
 output "name" {
-  value = module.gke_cluster.name
+  description = "Cluster name"
+  value       = module.gke_cluster.name
 }
 
 output "network_policy_enabled" {
-  value = module.gke_cluster.network_policy_enabled
+  description = "Whether network policy enabled"
+  value       = module.gke_cluster.network_policy_enabled
 }
 
 output "region" {
-  value = module.gke_cluster.region
+  description = "Cluster region"
+  value       = module.gke_cluster.region
 }
 
 output "release_channel" {
-  value = module.gke_cluster.release_channel
+  description = "The release channel of this cluster"
+  value       = module.gke_cluster.release_channel
 }
 
 output "service_account" {
-  value = module.gke_cluster.service_account
+  description = "The service account to default running nodes as if not overridden in `node_pools`."
+  value       = module.gke_cluster.service_account
 }
 
 output "type" {
-  value = module.gke_cluster.type
+  description = "Cluster type (regional / zonal)"
+  value       = module.gke_cluster.type
 }
 
 output "zones" {
-  value = module.gke_cluster.zones
+  description = "List of zones in which the cluster resides"
+  value       = module.gke_cluster.zones
 }

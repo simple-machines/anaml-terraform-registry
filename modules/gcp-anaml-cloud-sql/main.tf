1db93342f592869e8df6e1b42e4632b56c6f9487
Adam Evans <aevans@simplemachines.com.au>
2022-07-08 15:22:17 +1000

    Initial commit of gcp-anaml-cloud-sql module

---
+++ modules/gcp-anaml-cloud-sql/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_version = ">= 1.1"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.3"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.53.0, < 5.0.0"
+    }
+  }
+}
+
+resource "random_id" "anaml_db_name_suffix" {
+  for_each    = toset(var.postgresql_deploy_versions)
+  byte_length = 4
+}
+
+resource "random_password" "anaml_postgres_password" {
+  length  = 32
+  special = false
+}
+
+resource "google_sql_database_instance" "anaml_postgres_instance" {
+  for_each         = toset(var.postgresql_deploy_versions)
+  name             = "${var.name_prefix}${random_id.anaml_db_name_suffix[each.key].hex}"
+  region           = var.region
+  database_version = "POSTGRES_${each.key}"
+  settings {
+    tier      = var.tier
+    disk_size = var.disk_size
+    ip_configuration {
+      ipv4_enabled    = var.ip_configuration_ipv4_enabled
+      private_network = var.ip_configuration_private_network_id
+    }
+    insights_config {
+      query_insights_enabled  = var.insights_config_query_insights_enabled
+      query_string_length     = var.insights_config_query_string_length
+      record_application_tags = var.insights_config_record_application_tags
+      record_client_address   = var.insights_config_record_client_address
+    }
+  }
+
+  deletion_protection = var.deletion_protection
+  project             = var.project_name
+}
+
+# resource "google_sql_user" "users" {
+#   for_each        = toset(var.postgresql_deploy_versions)
+#   name            = "postgres"
+#   instance        = google_sql_database_instance.anaml_postgres_instance[each.key].name
+#   password        = random_password.anaml_postgres_password.result
+#   deletion_policy = "ABANDON"
+# }
---
+++ modules/gcp-anaml-cloud-sql/variables.tf
@@ -0,0 +1,64 @@
+variable "postgresql_deploy_versions" {
+  type        = list(any)
+  description = "Versions of PostgreSQL to deploy. Can facilitate with upgrades."
+  default     = ["13"]
+}
+
+variable "name_prefix" {
+  type = string
+  default = "anaml-"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP region"
+}
+
+variable "project_name" {
+  type        = string
+  description = "GCP project name."
+}
+
+variable "ip_configuration_private_network_id" {
+  type = string
+}
+
+variable "ip_configuration_ipv4_enabled" {
+  type = bool
+  default = false
+}
+
+variable "tier" {
+  type = string
+  default = "db-custom-2-8192"
+}
+
+variable "disk_size" {
+  type = number
+  default = 20
+}
+
+variable "deletion_protection" {
+  type = bool
+  default = false
+}
+
+variable "insights_config_query_insights_enabled" {
+  type = bool
+  default = true
+}
+
+variable "insights_config_query_string_length" {
+  type = number
+  default = 1024
+}
+
+variable "insights_config_record_application_tags" {
+  type = bool
+  default = false
+}
+
+variable "insights_config_record_client_address" {
+  type = bool
+  default = false
+}

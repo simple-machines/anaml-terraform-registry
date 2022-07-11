801184e4ddd7fbe421655bbc2be30a3f34c7d18e
Adam Evans <aevans@simplemachines.com.au>
2022-07-11 15:09:39 +1000

    Initial Google Cloud SQL auth proxy module commit.
    This deploys a standalone Google Cloud SQL auth proxy

---
+++ modules/gcp-cloudsql-proxy/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_version = ">= 1.1"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.11"
+    }
+  }
+}
+
+locals {
+  deployment_labels = merge({
+    "app.kubernetes.io/name"       = "cloudsql-proxy"
+    "app.kubernetes.io/version"    = var.gcp_cloudsql_proxy_version
+    "app.kubernetes.io/component"  = "database"
+    "app.kubernetes.io/part-of"    = "anaml"
+    "app.kubernetes.io/created-by" = "terraform"
+  }, var.kubernetes_deployment_labels)
+}
+
+resource "kubernetes_deployment" "default" {
+  metadata {
+    name      = var.kubernetes_deployment_name
+    namespace = var.kubernetes_namespace
+    labels = local.deployment_labels
+  }
+  spec {
+    selector {
+      match_labels = local.deployment_labels
+    }
+    template {
+      metadata {
+        name      = var.kubernetes_deployment_name
+        namespace = var.kubernetes_namespace
+        labels = local.deployment_labels
+      }
+      spec {
+        service_account_name = var.kubernetes_service_account
+        node_selector = var.kubernetes_node_selector
+        container {
+          name  = "cloudsql-proxy"
+          image = "${var.gcp_cloudsql_image_repository}:${var.gcp_cloudsql_proxy_version}"
+          command = flatten([
+            ["/cloud_sql_proxy"],
+            ["-dir=/cloudsql"],
+            ["-instances=${join(",", var.gcp_cloudsql_instances)}=tcp:5432"],
+            ["-log_debug_stdout"],
+            ["-use_http_health_check"],
+            ["-verbose"],
+            var.gcp_cloudsql_structured_logs ? ["-structured_logs"] : [],
+            var.gcp_cloudsql_use_private_ip ? ["-ip_address_types=PRIVATE"] : []
+          ])
+          security_context {
+            run_as_non_root = true
+            run_as_group    = 2000
+            run_as_user     = 1000
+          }
+        }
+      }
+    }
+  }
+}
---
+++ modules/gcp-cloudsql-proxy/variables.tf
@@ -0,0 +1,65 @@
+variable "gcp_cloudsql_proxy_version" {
+  type = string
+  default = "1.31.0"
+}
+
+variable "kubernetes_deployment_name" {
+  type    = string
+  default = "cloudsql-proxy"
+}
+
+variable "kubernetes_deployment_replicas" {
+  type    = number
+  default = 1
+}
+
+variable "kubernetes_image_pull_policy" {
+  type    = string
+  default = "IfNotPresent"
+}
+
+variable "kubernetes_namespace" {
+  type = string
+}
+
+variable "kubernetes_node_selector" {
+  type     = map(string)
+  default  = null
+  nullable = true
+}
+
+variable "kubernetes_service_account" {
+  type = string
+  default = null
+}
+
+variable "kubernetes_deployment_labels" {
+  type        = map(string)
+  default     = null
+  description = "Kubernetes labels to set if any. These values will be merged with the defaults"
+}
+
+
+# Available - See https://github.com/GoogleCloudPlatform/cloudsql-proxy
+#  - gcr.io/cloudsql-docker/gce-proxy
+#  - us.gcr.io/cloudsql-docker/gce-proxy
+#  - eu.gcr.io/cloudsql-docker/gce-proxy
+#  - asia.gcr.io/cloudsql-docker/gce-proxy
+variable "gcp_cloudsql_image_repository" {
+  type = string
+  default = "gcr.io/cloudsql-docker/gce-proxy"
+}
+
+variable "gcp_cloudsql_use_private_ip" {
+  type = bool
+  default = true
+}
+
+variable "gcp_cloudsql_instances" {
+  type = list(string)
+}
+
+variable "gcp_cloudsql_structured_logs" {
+  type = bool
+  default = true
+}

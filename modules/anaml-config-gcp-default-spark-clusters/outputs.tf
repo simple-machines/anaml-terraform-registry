2cfb09a91a85859c378bd33658bd3cba04785de4
Adam Evans <aevans@simplemachines.com.au>
2022-07-19 17:54:14 +1000

    Changing outputs to be an object.
    Downstream consumers require the numeric id not name and using a
    object makes it easier to group the values

--- modules/anaml-config-gcp-default-spark-clusters/outputs.tf
+++ modules/anaml-config-gcp-default-spark-clusters/outputs.tf
@@ -1,11 +1,21 @@
 output "spark_preview_cluster_name" {
-  value = anaml-operations_cluster.spark_on_k8s_preview_cluster.name
+  value = {
+    name = anaml-operations_cluster.spark_on_k8s_preview_cluster.name
+    id = anaml-operations_cluster.spark_on_k8s_preview_cluster.id
+  }
 }
 
-output "spark_job_cluster_name" {
-  value = anaml-operations_cluster.spark_on_k8s_job_cluster.name
+output "spark_job_cluster" {
+  value = {
+    name = anaml-operations_cluster.spark_on_k8s_job_cluster.name
+    id = anaml-operations_cluster.spark_on_k8s_job_cluster.id
+  }
 }
+
 
 output "spark_job_event_store_cluster_name" {
-  value = anaml-operations_cluster.event_store_cluster.name
+  value = {
+    name = anaml-operations_cluster.event_store_cluster.name
+    id = anaml-operations_cluster.event_store_cluster.id
+  }
 }

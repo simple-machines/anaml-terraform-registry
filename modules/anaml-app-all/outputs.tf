eab04b7b6aa57133ae80aa952cf1d57d39164342
Adam Evans <aevans@simplemachines.com.au>
2022-07-19 15:48:08 +1000

    Updated anaml-all to output the service account used.
    This is useful for downstream modules using the service-account as
    there inputs

--- modules/anaml-app-all/outputs.tf
+++ modules/anaml-app-all/outputs.tf
@@ -1,3 +1,8 @@
 output "anaml_api_url" {
   value = module.anaml-server.anaml_api_url
 }
+
+output "kubernetes_service_account" {
+  description = "The Kubernetes service account used for deployments"
+  value = var.kubernetes_service_account_name
+}

bb47b43b9b2867f105051e7931e8c8b917a1e536
Adam Evans <aevans@simplemachines.com.au>
2022-07-27 12:44:27 +1000

    Misc refactoring

--- modules/app-docs/outputs.tf
+++ modules/app-docs/outputs.tf
@@ -1,3 +1,7 @@
 output "internal_url" {
-  value = "http://${kubernetes_service.anaml_docs.metadata[0].name}.${kubernetes_service.anaml_docs.metadata[0].namespace}.svc.cluster.local"
+  value = format(
+    "http://%s.%s.svc.cluster.local",
+    kubernetes_service.anaml_docs.metadata.0.name,
+    kubernetes_service.anaml_docs.metadata.0.namespace
+  )
 }

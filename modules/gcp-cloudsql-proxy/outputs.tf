c6673c64f7fc0f8ae68be06b5d3639e1348a1f99
Adam Evans <aevans@simplemachines.com.au>
2022-08-01 16:27:42 +1000

    Updating sql-proxy outputs

--- modules/gcp-cloudsql-proxy/outputs.tf
+++ modules/gcp-cloudsql-proxy/outputs.tf
@@ -1,8 +1,7 @@
-output "internal_url" {
+output "host" {
   value =  var.kubernetes_service_enable ? format(
-    "%s.%s.svc.cluster.local:%s",
+    "%s.%s.svc.cluster.local",
     kubernetes_service.default.0.metadata.0.name,
-    kubernetes_service.default.0.metadata.0.namespace,
-    kubernetes_service.default.0.spec.port.0.port
+    kubernetes_service.default.0.metadata.0.namespace
   ) : null
 }

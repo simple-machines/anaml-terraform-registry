c7e24892557adcf4fb783996a911113da2b71dfd
Adam Evans <aevans@simplemachines.com.au>
2022-08-01 16:25:44 +1000

    Updating output url for cloud sql proxy

--- modules/gcp-cloudsql-proxy/outputs.tf
+++ modules/gcp-cloudsql-proxy/outputs.tf
@@ -1,7 +1,8 @@
 output "internal_url" {
   value =  var.kubernetes_service_enable ? format(
-    "http://%s.%s.svc.cluster.local",
+    "%s.%s.svc.cluster.local:%s",
     kubernetes_service.default.0.metadata.0.name,
-    kubernetes_service.default.0.metadata.0.namespace
+    kubernetes_service.default.0.metadata.0.namespace,
+    kubernetes_service.default.0.spec.port.0.port
   ) : null
 }

6455eba2957529b130777ed5553bee90d5485df4
Adam Evans <aevans@simplemachines.com.au>
2022-08-02 14:57:58 +1000

    Added internal_url output to app-ui for consitency

--- modules/app-ui/outputs.tf
+++ modules/app-ui/outputs.tf
@@ -9,3 +9,11 @@ output "kubernetes_service_port_name" {
 output "kubernetes_service_port_number" {
   value = kubernetes_service.anaml_ui.spec.0.port.0.port
 }
+
+output "internal_url" {
+  value = format(
+    "http://%s.%s.svc.cluster.local",
+    kubernetes_service.anaml_ui.metadata.0.name,
+    kubernetes_service.anaml_ui.metadata.0.namespace
+  )
+}

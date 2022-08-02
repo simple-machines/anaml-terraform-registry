5f07827d8851b6e94fafb2a0833f9cc4544e534b
Adam Evans <aevans@simplemachines.com.au>
2022-08-02 14:34:02 +1000

    Updated app-all to output app-server service name

--- modules/app-all/outputs.tf
+++ modules/app-all/outputs.tf
@@ -6,3 +6,7 @@ output "kubernetes_service_account" {
   description = "The Kubernetes service account used for deployments"
   value       = var.kubernetes_service_account_name
 }
+
+output "kubernetes_service_name_anaml_server" {
+  value = module.anaml-server.kubernetes_service_name
+}
--- modules/app-server/outputs.tf
+++ modules/app-server/outputs.tf
@@ -2,6 +2,10 @@ output "anaml_admin_api_kubernetes_secret_name" {
   value = kubernetes_secret.anaml_server_admin_api_auth.metadata[0].name
 }
 
+output "kubernetes_service_name" {
+  value = kubernetes_service.anaml_server.metadata.0.name
+}
+
 output "internal_url" {
   value = "http://anaml-server.${var.kubernetes_namespace}.svc.cluster.local:8080"
 }

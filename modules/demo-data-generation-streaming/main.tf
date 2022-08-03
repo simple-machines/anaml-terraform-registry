e72e0c1fdfb576aabb21ac0d846e0bfdb739ee87
Adam Evans <aevans@simplemachines.com.au>
2022-08-03 15:05:30 +1000

    Adding config object to streaming data module

--- modules/demo-data-generation-streaming/main.tf
+++ modules/demo-data-generation-streaming/main.tf
@@ -20,86 +20,103 @@ locals {
     "app.kubernetes.io/part-of"    = "anaml"
     "app.kubernetes.io/created-by" = "terraform"
   }, var.kubernetes_deployment_labels)
+
+  kafka_config = merge({
+    "bootstrap.servers": var.kafka_bootstrap_servers
+    "key.serializer":  "org.apache.kafka.common.serialization.VoidSerializer"
+    "value.serializer": "io.confluent.kafka.serializers.KafkaAvroSerializer"
+    "schema.registry.url": var.kafka_schema_registry_url
+  }, var.kafka_additional_config)
+
 }
+
+
 # resource "kubernetes_deployment" "kafka_data_generator" {
 #   metadata {
 #     name      = "anaml-producer-demo"
-#     namespace = "anaml"
-#     labels = {
-#       name = "anaml-producer-demo"
-#     }
+#     namespace = var.kubernetes_namespace
+#     labels = local.deployment_labels
 #   }
 #   spec {
 #     selector {
 #       match_labels = {
-#         app = "anaml-producer-demo"
+#         "app.kubernetes.io/name" = local.deployment_labels["app.kubernetes.io/name"]
 #       }
 #     }
 #     template {
 #       metadata {
 #         name      = "anaml-producer-demo"
 #         namespace = "anaml"
-#         labels = {
-#           name = "anaml-producer-demo"
-#           app  = "anaml-producer-demo"
-#         }
+#         labels = local.deployment_labels
 #       }
 #       spec {
 #         service_account_name = "svc-data-generator"
+
+#         # volume {
+#         #   name = "config"
+#         #   config_map {
+#         #     name = kubernetes_config_map.producer_demo_config.metadata.0.name
+#         #   }
+#         # }
+
 #         volume {
-#           name = "config"
-#           config_map {
-#             name = kubernetes_config_map.producer_demo_config.metadata.0.name
+#           name = "data-generation-volume"
+#           persistent_volume_claim {
+#             claim_name = kubernetes_persistent_volume_claim.data_generation_volume.metadata.0.name
 #           }
 #         }
-#         node_selector = {
-#           node_pool = "anaml-node-pool"
-#         }
+
+#         node_selector = var.kubernetes_node_selector
+
 #         container {
 #           name              = "anaml-producer-demo"
-#           image             = "${var.gcr_repository_url}/anaml-producer-demo:latest"
-#           image_pull_policy = "Always"
+#           image             = "${var.container_registry}/anaml-producer-demo:${anaml_producer_demo_version}"
+#           image_pull_policy = var.kubernetes_image_pull_policy == null ? (var.anaml_server_version == "latest" ? "Always" : "IfNotPresent") : var.kubernetes_image_pull_policy
+
 #           env {
 #             name  = "ANAML_GS_PROJECT_ID"
-#             value = var.project_name
+#             value = var.google_project_id
 #           }
+
 #           env {
 #             name  = "ANAML_GS_BUCKET"
-#             value = "anaml-dev-warehouse"
+#             value = var.google_bucket
 #           }
+
 #           env {
 #             name  = "GS_CUSTOMER_SOURCE_PATH"
-#             value = "vapour/customers"
+#             value = var.customer_source_path
 #           }
+
 #           env {
 #             name  = "DATA_DIR"
 #             value = "/app/data"
 #           }
+
 #           env {
 #             name  = "JAVA_OPTS"
 #             value = "-Dconfig.file=/config/application.properties"
 #           }
+
 #           env {
 #             name  = "GOOGLE_APPLICATION_CREDENTIALS"
 #             value = "/config/gcloud.json"
 #           }
+
 #           command = ["/app/start.sh"]
+
 #           volume_mount {
 #             name       = "data-generation-volume"
 #             mount_path = "/app/data"
 #           }
+
 #           volume_mount {
 #             name       = "config"
 #             mount_path = "/config"
 #             read_only  = true
 #           }
 #         }
-#         volume {
-#           name = "data-generation-volume"
-#           persistent_volume_claim {
-#             claim_name = kubernetes_persistent_volume_claim.producer_data_generation_volume.metadata.0.name
-#           }
-#         }
+
 #       }
 #     }
 #   }
@@ -121,30 +138,18 @@ resource "kubernetes_persistent_volume_claim" "data_generation_volume" {
   }
 }
 
-# resource "kubernetes_config_map" "producer_demo_config" {
-#   metadata {
-#     name      = "producer-demo-config"
-#     namespace = "anaml"
-#   }
 
-#   data = {
-#     "application.properties" = <<EOF
-# # Required connection configs for Kafka producer, consumer, and admin
-# bootstrap.servers=${var.bootstrap_server}
-# security.protocol=SASL_SSL
-# sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username='${data.google_secret_manager_secret_version.producer_demo_kafka_key.secret_data}' password='${data.google_secret_manager_secret_version.producer_demo_kafka_secret.secret_data}';
-# sasl.mechanism=PLAIN
-# key.serializer=org.apache.kafka.common.serialization.VoidSerializer
-# value.serializer=io.confluent.kafka.serializers.KafkaAvroSerializer
-# schema.registry.url=https://psrc-41vyv.australia-southeast1.gcp.confluent.cloud
-# basic.auth.credentials.source=USER_INFO
-# basic.auth.user.info=${data.google_secret_manager_secret_version.producer_demo_schema_registry_key.secret_data}:${data.google_secret_manager_secret_version.producer_demo_schema_registry_secret.secret_data}
-# acks=all
-#     EOF
 
-#     "gcloud.json" = data.google_secret_manager_secret_version.producer_demo_gcloud_credentials_secret.secret_data
-#   }
-# }
+
+resource "kubernetes_config_map" "producer_demo_config" {
+  metadata {
+    name      = "anaml-demo-streaming-data-generationr"
+    namespace = var.kubernetes_namespace
+    labels    = { for k, v in local.deployment_labels : k => v if k != "app.kubernetes.io/version" }
+  }
+
+  data = join("\n", [ for k,v in locals.kafka_config : => "${k}=${v}" ])
+}
 
 
 # data "google_secret_manager_secret_version" "producer_demo_kafka_key" {
--- modules/demo-data-generation-streaming/variables.tf
+++ modules/demo-data-generation-streaming/variables.tf
@@ -4,6 +4,37 @@ variable "anaml_producer_demo_version" {
   nullable    = false
 }
 
+variable "container_registry" {
+  type        = string
+  default     = "australia-southeast1-docker.pkg.dev/anaml-release-artifacts/docker"
+  description = "The container registry to use to fetch the anaml-docs container"
+  nullable    = false
+}
+
+variable "google_project_id" {
+  type = string
+}
+
+variable "google_bucket" {
+  type = string
+}
+
+variable "customer_source_path" {
+  type = string
+}
+
+variable "kafka_bootstrap_servers" {
+  type = string
+}
+
+variable "kafka_additional_config" {
+  type = map(string)
+}
+
+variable "kafka_schema_registry_url" {
+  type = string
+}
+
 variable "kubernetes_deployment_labels" {
   type        = map(string)
   default     = null
@@ -13,4 +44,22 @@ variable "kubernetes_deployment_labels" {
 variable "kubernetes_namespace" {
   type     = string
   nullable = false
+}
+
+variable "kubernetes_node_selector" {
+  type        = map(string)
+  default     = null
+  description = "(Optional) NodeSelector is a selector which must be true for the pod to fit on a node. Selector which must match a node's labels for the pod to be scheduled on that node. For more info see [Kubernetes reference](http://kubernetes.io/docs/user-guide/node-selection)."
+  nullable    = true
+}
+
+variable "kubernetes_image_pull_policy" {
+  type        = string
+  description = " (Optional) Image pull policy. One of Always, Never, IfNotPresent. Defaults to Always if `anaml_server_version` is set to`latest`, or IfNotPresent otherwise. For more info see [Kubernetes reference](http://kubernetes.io/docs/user-guide/images#updating-images)"
+  default     = null
+
+  validation {
+    condition     = var.kubernetes_image_pull_policy == null ? true : contains(["Always", "Never", "IfNotPresent"], var.kubernetes_image_pull_policy)
+    error_message = "The kubernetes_image_pull_policy value must be one of Always, Nerver or IfNotPresent."
+  }
 }

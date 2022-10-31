13ebd6515bf3e1684bcb25e7f2052a603338660e
Adam Evans <aevans@simplemachines.com.au>
2022-10-31 15:39:55 +1100

    Change spark server urls to be a map

--- modules/app-spark-server/outputs.tf
+++ modules/app-spark-server/outputs.tf
@@ -9,13 +9,13 @@ output "spark_history_server_internal_url" {
 }
 
 output "anaml_spark_server_internal_url" {
-  value = [
-    for service in kubernetes_service.anaml_spark_server_service : format(
+  value = {
+    for service in kubernetes_service.anaml_spark_server_service : service.metadata.0.name => format(
       "%s://%s.%s.svc.cluster.local:%s",
       var.ssl_kubernetes_secret_pkcs12_keystore != null ? "https" : "http",
       service.metadata.0.name,
       service.metadata.0.namespace,
       service.spec[0].port[0].port
     )
-  ]
+  }
 }

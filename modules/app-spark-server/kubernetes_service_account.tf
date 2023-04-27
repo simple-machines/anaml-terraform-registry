da3b7ec39f82ec5ce1fdb0a76746dae051c5e69c
Iain Cardnell <idc101@users.noreply.github.com>
2023-04-27 14:38:28 +0100

    extra kubernetes svc account permissions required by spark 3.4

--- modules/app-spark-server/kubernetes_service_account.tf
+++ modules/app-spark-server/kubernetes_service_account.tf
@@ -16,21 +16,8 @@ resource "kubernetes_role" "default" {
   }
   rule {
     api_groups = [""]
-    resources  = ["pods", "configmaps", "services"]
-    verbs      = ["get", "list", "watch", "create", "delete"]
-  }
-}
-
-resource "kubernetes_role" "delete_spark_pods" {
-  metadata {
-    name      = "delete-spark-pods"
-    namespace = var.kubernetes_namespace
-    labels    = { for k, v in local.anaml_spark_server_labels : k => v if k != "app.kubernetes.io/version" }
-  }
-  rule {
-    api_groups = [""]
-    resources  = ["pods"]
-    verbs      = ["deletecollection"]
+    resources  = ["pods", "configmaps", "services", "persistentvolumeclaims"]
+    verbs      = ["get", "list", "watch", "create", "delete", "deletecollection"]
   }
 }
 
@@ -44,30 +31,6 @@ resource "kubernetes_role_binding" "spark" {
     api_group = "rbac.authorization.k8s.io"
     kind      = "Role"
     name      = var.kubernetes_role_spark_driver_executor_name
-  }
-
-  dynamic "subject" {
-    for_each = toset(compact([var.kubernetes_service_account_deployment, var.kubernetes_service_account_spark_driver_executor]))
-
-    content {
-      kind      = "ServiceAccount"
-      name      = subject.value
-      namespace = var.kubernetes_namespace
-    }
-  }
-}
-
-
-resource "kubernetes_role_binding" "delete_spark_pods" {
-  metadata {
-    name      = "delete_spark_pods_binding"
-    namespace = var.kubernetes_namespace
-    labels    = { for k, v in local.anaml_spark_server_labels : k => v if k != "app.kubernetes.io/version" }
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "Role"
-    name      = kubernetes_role.delete_spark_pods.metadata.0.name
   }
 
   dynamic "subject" {
